7b88f770b3238c0284920e4722f7aa6946a8feae
Josh David Miller <josh@joshdmiller.com>
2016-07-07 16:11:18 -0700

    feat(redux): add redux-logger middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,17 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createLogger from 'redux-logger';
+
+const logger = createLogger({
+  collapsed: true,
+});
 
 const reducer = ( state = {}, action ) => state;
 const initialState = {};
-const store = createStore( reducer, initialState );
+const middleware = applyMiddleware(
+  logger
+);
+
+const store = createStore( reducer, initialState, middleware );
 
 export default store;
 
