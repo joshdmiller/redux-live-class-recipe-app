c3d33bd0bf1d992460397b12626260d7c5f7a2a0
Josh David Miller <josh@joshdmiller.com>
2016-07-13 15:40:30 -0700

    feat: add material-ui library, generic AppBar

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
 
 import Greeting from '../greeting';
 import store from '../../store';
 
+// dirty hack because material-ui requires a custom event type
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
 export default () => {
   return (
     <Provider store={store}>
-      <Greeting />
+      <MuiThemeProvider>
+        <div>
+          <AppBar
+            title="Alfred"
+          />
+          <Greeting />
+        </div>
+      </MuiThemeProvider>
     </Provider>
   );
 }
--- src/components/app/spec.js
+++ src/components/app/spec.js
@@ -2,13 +2,14 @@ import React from 'react';
 import test from 'tape';
 import { shallow } from 'enzyme';
 import { Provider } from 'react-redux';
+import AppBar from 'material-ui/AppBar';
 
 import App from './';
 import Greeting from '../greeting';
 
 test( 'App', t => {
   let expected, actual;
-  t.plan( 2 );
+  t.plan( 3 );
 
   const wrapper = shallow( <App /> );
   expected = true;
@@ -20,5 +21,10 @@ test( 'App', t => {
   actual = wrapper.find( Greeting ).length;
 
   t.equals( actual, expected, 'renders a Greeting component' );
+
+  expected = 1;
+  actual = wrapper.find( AppBar ).length;
+
+  t.equals( actual, expected, 'renders the AppBar' );
 });
 
